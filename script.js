3f3bcba6f5a7872e6f7b08dc60cf9ad8d27c852d
dhanraj-mohan101 <dhanu.shine88@gmail.com>
2026-07-22 22:38:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// Back To Top Button
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", function () {
+
+    if (window.scrollY > 300) {
+
+        topBtn.style.display = "block";
+
+    } else {
+
+        topBtn.style.display = "none";
+
+    }
+
+});
+
+topBtn.addEventListener("click", function () {
+
+    window.scrollTo({
+
+        top: 0,
+
+        behavior: "smooth"
+
+    });
+
+});
+gtag('event', 'contact_form_submit', {
+    event_category: 'Contact',
+    event_label: 'Portfolio Contact Form'
+});
